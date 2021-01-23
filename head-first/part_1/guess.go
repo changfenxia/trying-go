58174b0c8f107985a2fe83b630578844f13bc353
Artem <artyom.arbatskiy@gmail.com>
2021-01-23 22:14:38 +0800

    Add something to guess.go

--- head-first/part_1/guess.go
+++ head-first/part_1/guess.go
@@ -1,24 +1,49 @@
 package main
 
 import (
-	// "fmt"
-	// "bufio"
-	// "os"
-	// "strconv"
+	"bufio"
+	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
+var reader = bufio.NewReader(os.Stdin)
+
 func main() {
-	// get random number
+	// get random number - DONE
 	s1 := rand.NewSource((time.Now().UnixNano()))
 	r1 := rand.New(s1)
 	num := r1.Intn(10)
-	num = 5
 
-	// get user input
-	input := 5
-	// print both
-	println(num, input)
-	// implement loop & conditions
+	guess := getInput()
+	tries := 1
+
+	for ;guess != num; {
+		if (guess > num) {
+			fmt.Println("Wrong! Too high! Try again!")
+		} else {
+			fmt.Println("Yay! Too low... Try again")
+		}
+		guess = getInput()
+		tries++
+	}
+	
+	fmt.Println("You are right!")
+	fmt.Println("The number was:", num)
+	fmt.Println("Your guess:", guess)
+	fmt.Println("It took", tries, "tries")
+}
+
+func getInput() int{
+	fmt.Println("Enter number:")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	inputNum, _ := strconv.Atoi(strings.TrimSpace(input))
+	return inputNum
 }
--- head-first/part_1/main.go
+++ head-first/part_1/main.go
@@ -6,14 +6,6 @@ import (
 )
 
 func main() {
-	babe := "Carrie"
-	var quantity int
-	var length, width = 1.2, 4.23
-	quantity = 54
-	var customerName string
-	customerName = "ЖОПА"
-	fmt.Println(reflect.TypeOf(false))
-	fmt.Println(length, width)
-	fmt.Println(quantity * int (length))
-	fmt.Println(customerName + babe)
+	num := 1234
+	fmt.Println(reflect.TypeOf(fmt.Sprint(num)))
 }
